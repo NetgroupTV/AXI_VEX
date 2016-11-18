6d67df9250fb76fac30fbcd412b87606ef9b8e43
marcux95 <marco.spaziani.b@gmail.com>
2016-11-18 09:27:12 +0100

    Update dual_port_ram.vhd

--- dual_port_ram.vhd
+++ dual_port_ram.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 entity dual_port_ram is
     generic (
     
-    LENGTH : natural := 8;
+    LENGTH : natural := 8; -- data bus width
     WIDTH : natural := 8
     );
     
@@ -22,19 +22,21 @@ entity dual_port_ram is
 end dual_port_ram;
 
 architecture behavioral of dual_port_ram is
-  type registerFile is array(0 to WIDTH-1) of std_logic_vector(LENGTH-1 downto 0);
-  signal registers : registerFile;
+  
+    type registerFile is array(0 to WIDTH-1) of std_logic_vector(LENGTH-1 downto 0);
+    signal registers : registerFile;
+
 begin
   regFile : process (clk) is
   begin
     if rising_edge(clk) then
-      -- Read A and B before bypass
+     
       data_out <= registers(to_integer(unsigned(read_add)));
       
       -- Write and bypass
       if we = '1' then
         registers(to_integer(unsigned(write_add))) <= data_in;  -- Write
-        if read_add = write_add then  -- Bypass for read A
+        if read_add = write_add then  -- Bypass for read 
           data_out <= data_in;
         end if;
        
