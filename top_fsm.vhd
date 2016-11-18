f4aa73121e8d5772004240aeb266f2992db91faa
marcux95 <marco.spaziani.b@gmail.com>
2016-11-18 15:31:22 +0100

    Update top_fsm.vhd

--- top_fsm.vhd
+++ top_fsm.vhd
@@ -1,18 +1,15 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-
-
 entity top_fsm is
-
 generic (
-        
+
      LENGTH : natural := 8; --data bus width
      WIDTH : natural := 8
 );
-    
+
   port(
- 
+
     data_in       : in  std_logic_vector(LENGTH-1 downto 0);
     data_valid : in  std_logic;
     clk         : in  std_logic;
@@ -24,53 +21,14 @@ end top_fsm;
 
 architecture structural of top_fsm is
 
-component dual_port_ram
-    
-  port(
-    data_out        : out std_logic_vector(LENGTH-1 downto 0);
-
-    data_in       : in  std_logic_vector(LENGTH-1 downto 0);
-    we : in  std_logic;
-    read_add     : in  std_logic_vector(WIDTH-1 downto 0);
-
-    write_add : in  std_logic_vector(WIDTH-1 downto 0);
-    clk         : in  std_logic
-    );
-
-end component;
-
-component counter
-   
-    port (
-        clk : in std_logic;
-        rst : in std_logic;
-        Q : out std_logic_vector(WIDTH-1 downto 0));
-    end component;
-
 signal write_address : std_logic_vector(WIDTH-1 downto 0);
 
 
 begin
 
-CNT : counter port map (
+CNT : entity work.counter port map (clk,rst_cnt,write_address);
 
-        clk => clk,
-        rst => rst_cnt,
-        Q (WIDTH -1 downto 0) => write_address(WIDTH -1 downto 0)
-        );
-        
-RAM : dual_port_ram port map (
-        
-        we => data_valid,
-        clk => clk,
-        write_add(WIDTH -1 downto 0) => write_address(WIDTH -1 downto 0),
-        data_out(LENGTH-1 downto 0) => data_out_t(LENGTH-1 downto 0),
-        data_in (LENGTH-1 downto 0) => data_in(LENGTH-1 downto 0),
-        read_add (WIDTH -1 downto 0) => add_read(WIDTH -1 downto 0)
-        );
-        
- 
- 
-        
+
+RAM : work.dual_port_ram port map (data_valid,clk,write_address,data_out,data_in,read_add);
 
 end structural;
