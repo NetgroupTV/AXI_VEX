ee1f62c141ab9a158c4dd1427b6983ee05b691ab
marcux95 <marco.spaziani.b@gmail.com>
2016-11-19 10:45:24 +0100

    Update dual_port_ram.vhd

--- dual_port_ram.vhd
+++ dual_port_ram.vhd
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+USE ieee.std_logic_arith.all;
 
 entity dual_port_ram is
     generic (
@@ -14,33 +15,35 @@ entity dual_port_ram is
 
     data_in       : in  std_logic_vector(LENGTH-1 downto 0);
     we : in  std_logic;
-    read_add     : in  std_logic_vector(WIDTH-1 downto 0);
 
-    write_add : in  std_logic_vector(WIDTH-1 downto 0);
+    read_add     : in  integer ;
+
+    write_add : in  integer;
     clk         : in  std_logic
     );
 end dual_port_ram;
 
 architecture behavioral of dual_port_ram is
-  
-    type registerFile is array(0 to 2**(WIDTH-1)) of std_logic_vector(LENGTH-1 downto 0);
-    signal registers : registerFile;
+
+type registerFile is array(0 to 2**(WIDTH-1)) of std_logic_vector(LENGTH-1 downto 0);
+signal registers : registerFile;                                                     
 
 begin
-  regFile : process (clk) is
-  begin
-    if rising_edge(clk) then
-     
-      data_out <= registers(to_integer(unsigned(read_add)));
-      
-      -- Write and bypass
-      if we = '1' then
-        registers(to_integer(unsigned(write_add))) <= data_in;  -- Write
-        if read_add = write_add then  -- Bypass for read 
-          data_out <= data_in;
-        end if;
-       
-      end if;
+process(clk)
+begin
+    if(rising_edge(clk)) then
+            data_out<= registers(conv_integer(read_add));
     end if;
-  end process;
+end process;
+
+process(clk)
+begin
+        
+        if(rising_edge(clk)) then
+            if(we = '1') then
+                registers(conv_integer(write_add)) <= data_in;
+            end if;
+            
+    end if;
+end process;
 end behavioral;
